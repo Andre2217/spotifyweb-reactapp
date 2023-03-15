4e5809995eb0d3dd7815131d563576739e6c5509
Andre28ap <67607051+Andre2217@users.noreply.github.com>
2023-03-15 09:23:39 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Carrosel from './components/Carrosel';
+import Home from './components/Home';
+
+
+function App() {
+
+
+  return (
+    <div className="App">
+
+      <Home></Home>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Home.jsx
@@ -0,0 +1,13 @@
+import Carrosel from "./Carrosel";
+import Navbar from "./Navbar";
+
+function Home() {
+    return (
+        <>       
+         <Navbar></Navbar>
+         <Carrosel></Carrosel>
+        </>
+    )
+}
+
+export default Home;
