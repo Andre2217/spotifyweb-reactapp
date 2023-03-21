679b51e374a05235a4924019d5c900532de9f73c
yago rodrigues <yagodev3@gmail.com>
2023-03-21 20:21:50 -0300

    Ajustes CSS Navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
 import './App.css';
 import Home from './components/home/Home';
-import FAQ from './components/FAQ/FAQ';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-            <FAQ />
-        </Switch>
-      </Router>
+      <Home />
     </div>
   );
 }
-
 
 export default App;
