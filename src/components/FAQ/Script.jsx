e7dcd43bea0e03ee78860e3ffca0b5a40a2ec8dc
yago rodrigues <yagodev3@gmail.com>
2023-03-21 08:37:34 -0300

    Ajustes Script FAQ

--- src/components/FAQ/Script.jsx
+++ src/components/FAQ/Script.jsx
@@ -1,22 +1,20 @@
-function Script(){
-    return(
-        <div>
-             <script> //Script JS para FAQ
-                let respostas=document.querySelectorAll(".accordion");
-                    respostas.forEach((event)={">"}{
-                    event.addEventListener('click',()=>{
-                        if(event.classList.contains("active")){
-                            event.classList.remove("active");
-                        }
-                        else{
-                            event.classList.add("active");
-                            }
-                        })
-                    })
-            </script>
-        </div>
+import React, { useEffect } from "react";
 
-    )
+function Script() {
+  useEffect(() => {
+    let respostas = document.querySelectorAll(".accordion");
+    respostas.forEach((event) => {
+      event.addEventListener("click", () => {
+        if (event.classList.contains("active")) {
+          event.classList.remove("active");
+        } else {
+          event.classList.add("active");
+        }
+      });
+    });
+  }, []);
+
+  return <div></div>;
 }
 
 export default Script;
