c39ea54f331872a9b1fead5fb4a8c942dbf95edf
yago rodrigues <yagodev3@gmail.com>
2023-03-21 09:44:21 -0300

    Ajustes finais no componente Home, inserção de Header e ajustes CSS

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
 import './App.css';
-import Carrosel from './components/Carrosel';
-import Home from './components/Home';
-import Cards from './components/Cards';
-import Perguntas from './components/FAQ/Perguntas';
-import Respostas from './components/FAQ/Respostas';
-import Script from './components/FAQ/Script';
+import Home from './components/home/Home';
 
 function App() {
-
-
   return (
     <div className="App">
 
       <Home></Home>
-      <Cards/>
-      <Carrosel/>
-      <Perguntas/>
-      <Respostas/>
-      <Script/>
 
     </div>
   );
---
+++ src/components/home/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import logo from '../images/logo1.png';
+
+function Header() {
+return (
+    <header>
+        <img src={logo} alt="toca-play-logo" height="150" />
+    </header>
+    );
+};
+export default Header;
