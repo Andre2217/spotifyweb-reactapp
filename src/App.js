f767758e4f95380dad45a9671923701ce623df9d
Andre28ap <67607051+Andre2217@users.noreply.github.com>
2023-03-21 11:35:45 -0300

    Ajeitei o carrosel

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import Home from './components/home/Home';
 
-
 function App() {
   return (
     <div className="App">
--- src/components/home/Carrosel.jsx
+++ src/components/home/Carrosel.jsx
@@ -3,20 +3,24 @@ import {motion} from 'framer-motion';
 import image1 from '../images/1.jpg';
 import image2 from '../images/2.jpg';
 import image3 from '../images/3.jpg';
+import image4 from '../images/4.jpg';
+import image5 from '../images/5.jpg';
+import image6 from '../images/6.jpg';
 
-const images = [image1, image2, image3];
+
+const images = [image1, image2, image3, image4, image5, image6];
 
 function Carrosel() {
   const carrosel = useRef();
   const [width, setWidth] = useState(0)
 
   useEffect(() =>{
-    console.log(carrosel.current?.scrollWidth, carrosel.current?.offsetWidth)
+    //console.log(carrosel.current?.scrollWidth, carrosel.current?.offsetWidth)
     setWidth(carrosel.current?.scrollWidth - carrosel.current?.offsetWidth)
   }, [])
 
   return (
-    <div className="App">
+    <div className="carrosel-content">
       <motion.div ref={carrosel} className='carrosel' whileTap={{cursor: "grabbing"}}>
           <motion.div 
           className='inner'
