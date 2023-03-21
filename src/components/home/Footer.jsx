75ec84f5e68899082d250b95157c9e94c8f195e8
yago rodrigues <yagodev3@gmail.com>
2023-03-21 20:59:53 -0300

    Ajustes CSS Footer

--- src/components/home/Footer.jsx
+++ src/components/home/Footer.jsx
@@ -6,7 +6,7 @@ function Footer() {
                     <li class='li'>SAC: (11) 99999-8753</li>
                     <li class="li"><button title="Instagram"><i class="fa-brands fa-instagram fa-3x"></i></button></li>
                     <li class="li"><button title="Facebook"><i class="fa-brands fa-facebook fa-3x"></i></button></li>
-                    <li class="li"><button title="Instagram"><i class="fa-brands fa-twitter fa-3x"></i></button></li>
+                    <li class="li"><button title="Twitter"><i class="fa-brands fa-twitter fa-3x"></i></button></li>
                 </ul>
             </div>
         </div>
