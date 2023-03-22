48e8e648bcbc25e61364a2ea6265b2303a1609ba
yago rodrigues <yagodev3@gmail.com>
2023-03-22 09:28:45 -0300

    Instalação do React-Router-Dom v6.0 + Ajuste de sintaxe nas rotas

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import './App.css';
 import Home from './components/home/Home';
 import FAQPage from './components/FAQ/FAQPage';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router,Routes, Route} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/faq" component={FAQPage} />
-        </Switch>
-      </BrowserRouter>
+      <Router>
+        <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/faq" element={<FAQPage/>} />
+        </Routes>
+      </Router>
     </div>
   );
 }
