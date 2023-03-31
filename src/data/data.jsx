285d9adcad928684751de790926321f3d3f9e3db
Andre28ap <67607051+Andre2217@users.noreply.github.com>
2023-03-30 21:59:58 -0300

    Data das playlists

---
+++ src/data/data.jsx
@@ -0,0 +1,10 @@
+const data = [
+    { id: 1, nome: "Adele", capa: '../images/1.jpg', musica: [{id:1, cantor: "Adele"}] },
+    { id: 2, nome: "Queen", capa: '../images/2.jpg', musica: [{id:1, cantor: "Queen"}] },
+    { id: 3, nome: "Coldplay", capa: '../images/3.jpg', musica: [{id:1, cantor: "Coldplay"}] },
+    { id: 4, nome: "Beatles", capa: '../images/4.jpg', musica: [{id:1, cantor: "Beatles"}] },
+    { id: 5, nome: "MichaelJackson", capa: '../images/5.jpg', musica: [{id:1, cantor: "MichaelJackson"}] },
+    { id: 6, nome: "SeuJorge", capa: '../images/6.jpg', musica: [{id:1, cantor: "SeuJorge"}] }
+];
+
+export default data;
