9f4664f6ac28d35ddeb6498fabe9578612e6ffd6
Andre28ap <67607051+Andre2217@users.noreply.github.com>
2023-03-30 22:02:20 -0300

    Adicionei o route pra playlists

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Home from './components/home/Home';
 import FAQPage from './components/FAQ/FAQPage';
+import PlaylistPage from './components/Playlist/PlaylistPage';
 import { BrowserRouter as Router,Routes, Route} from "react-router-dom";
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Router>
         <Routes>
         <Route path="/" element={<Home/>} />
+        <Route path="/playlist/:id" element={<PlaylistPage/>} />
         <Route path="/faq" element={<FAQPage/>} />
         </Routes>
       </Router>
