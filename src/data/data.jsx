4420eb6ddd6aad77581c5c264efc1bfcb3bc6414
Andre28ap <67607051+Andre2217@users.noreply.github.com>
2023-04-03 10:53:53 -0300

    Acrescentei coisas no data das playslists

--- src/data/data.jsx
+++ src/data/data.jsx
@@ -1,10 +1,10 @@
 const data = [
-    { id: 1, nome: "Adele", capa: '../images/1.jpg', musica: [{id:1, cantor: "Adele"}] },
-    { id: 2, nome: "Queen", capa: '../images/2.jpg', musica: [{id:1, cantor: "Queen"}] },
-    { id: 3, nome: "Coldplay", capa: '../images/3.jpg', musica: [{id:1, cantor: "Coldplay"}] },
-    { id: 4, nome: "Beatles", capa: '../images/4.jpg', musica: [{id:1, cantor: "Beatles"}] },
-    { id: 5, nome: "MichaelJackson", capa: '../images/5.jpg', musica: [{id:1, cantor: "MichaelJackson"}] },
-    { id: 6, nome: "SeuJorge", capa: '../images/6.jpg', musica: [{id:1, cantor: "SeuJorge"}] }
+    { id: 1, nome: "Adele", capa: '../images/1.jpg', musica: [{id:1, cantor: "Adele", nome:"set fire to the rain", track:'../sounds/Set_fire_to_the_rain.mp3'}] },
+    { id: 2, nome: "Queen", capa: '../images/2.jpg', musica: [{id:1, cantor: "Queen",nome:"Bohemian Rhapsody", track:'../sounds/BohemianRhapsody.mp3'}] },
+    { id: 3, nome: "Coldplay", capa: '../images/3.jpg', musica: [{id:1, cantor: "Coldplay", nome:"Viva La Vida", track:'../sounds/VivaLaVida.mp3'}] },
+    { id: 4, nome: "Beatles", capa: '../images/4.jpg', musica: [{id:1, cantor: "Beatles", nome:"Here comes the sun", track:'../sounds/HereComesTheSun.mp3'}] },
+    { id: 5, nome: "MichaelJackson", capa: '../images/5.jpg', musica: [{id:1, cantor: "MichaelJackson", nome:"Billie Jean", track:'../sounds/BillieJean.mp3'}] },
+    { id: 6, nome: "SeuJorge", capa: '../images/6.jpg', musica: [{id:1, cantor: "SeuJorge", nome:"Amiga da minha mulher", track:'../sounds/Amiga_da_minha_mulher.mp3'}] }
 ];
 
 export default data;
