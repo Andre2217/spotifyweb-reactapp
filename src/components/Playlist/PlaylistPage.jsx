25c05a0a3fe603b346e6f996b680efa829c89c5d
Andre28ap <67607051+Andre2217@users.noreply.github.com>
2023-04-03 10:55:15 -0300

    coloquei pra sair som ao clicar na imagem

--- src/components/Playlist/PlaylistPage.jsx
+++ src/components/Playlist/PlaylistPage.jsx
@@ -3,15 +3,19 @@ import data from '../../data/data';
 
 function PlaylistPage() {
     let { id } = useParams();
-    let playlist = data.find(p => p.id === id);
-    
+    let playlist = data.find(p => p.id == id);
+    const start = (track) => {
+        const audio = new Audio(track);
+        audio.play();
+      };
+
     return (
         <div>
             <h1>{playlist.nome}</h1>
             <p>{playlist.id}</p>
             <ul>
-                {playlist.musica.map(musica => (
-                    <li>{musica.id}{musica.cantor}</li>
+                {playlist.musica.map(musica => (//Só vai funcionar se tiver apenas um track, se tiver mais que um aí eu acho que buga.
+                    <><li>{musica.id}{musica.cantor} </li><img src={playlist.capa} alt="alt" onClick={() => start(musica.track)} /></>
                 ))}
             </ul>
         </div>
