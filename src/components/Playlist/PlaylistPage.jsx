d406e21e95526d0b103018dee0f6fea5309b0d41
Andre28ap <67607051+Andre2217@users.noreply.github.com>
2023-04-05 10:06:43 -0300

    Adicionei o pause da musica

--- src/components/Playlist/PlaylistPage.jsx
+++ src/components/Playlist/PlaylistPage.jsx
@@ -1,25 +1,46 @@
+import { useState } from 'react';
 import { useParams } from 'react-router-dom';
 import data from '../../data/data';
 
 function PlaylistPage() {
-    let { id } = useParams();
-    let playlist = data.find(p => p.id === id);
-    const start = (track) => {
-        const audio = new Audio(track);
-        audio.play();
-      };
+  let { id } = useParams();
+  let playlist = data.find(p => p.id == id);
 
-    return (
-        <div>
-            <h1>{playlist.nome}</h1>
-            <p>{playlist.id}</p>
-            <ul>
-                {playlist.musica.map(musica => (//Só vai funcionar se tiver apenas um track, se tiver mais que um aí eu acho que buga.
-                    <><li>{musica.id}{musica.cantor} </li><img src={playlist.capa} alt="alt" onClick={() => start(musica.track)} /></>
-                ))}
-            </ul>
+  const [currentTrack, setCurrentTrack] = useState(null);
+
+  const start = (track) => {
+    if (currentTrack) {
+      currentTrack.pause();
+    }
+    const audio = new Audio(track);
+    setCurrentTrack(audio);
+    audio.play();
+  };
+
+  const stop = () => {
+    if (currentTrack) {
+      currentTrack.pause();
+      setCurrentTrack(null);
+    }
+  };
+
+  return (
+    <div>
+      <h1>{playlist.nome}</h1>
+      <img src={playlist.capa} alt="alt" />
+      <ul>
+        {playlist.musica.map(musica => (
+          <li key={musica.id}>{musica.cantor}</li>
+        ))}
+      </ul>
+      {playlist.musica.map(musica => (
+        <div key={musica.id}>
+          <button onClick={() => start(musica.track)}>Play</button>
+          <button onClick={stop}>Stop</button>
         </div>
-    );
+      ))}
+    </div>
+  );
 }
 
 export default PlaylistPage;
