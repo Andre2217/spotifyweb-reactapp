5ece5015763c46bf0d5201506d770819409f573a
Andre28ap <67607051+Andre2217@users.noreply.github.com>
2023-04-05 10:09:33 -0300

    Adicionei mais 2 playlists e mais musicas

--- src/data/data.jsx
+++ src/data/data.jsx
@@ -1,10 +1,41 @@
 const data = [
-    { id: 1, nome: "Adele", capa: '../images/1.jpg', musica: [{id:1, cantor: "Adele", nome:"set fire to the rain", track:'../sounds/Set_fire_to_the_rain.mp3'}] },
-    { id: 2, nome: "Queen", capa: '../images/2.jpg', musica: [{id:1, cantor: "Queen",nome:"Bohemian Rhapsody", track:'../sounds/BohemianRhapsody.mp3'}] },
-    { id: 3, nome: "Coldplay", capa: '../images/3.jpg', musica: [{id:1, cantor: "Coldplay", nome:"Viva La Vida", track:'../sounds/VivaLaVida.mp3'}] },
-    { id: 4, nome: "Beatles", capa: '../images/4.jpg', musica: [{id:1, cantor: "Beatles", nome:"Here comes the sun", track:'../sounds/HereComesTheSun.mp3'}] },
-    { id: 5, nome: "MichaelJackson", capa: '../images/5.jpg', musica: [{id:1, cantor: "MichaelJackson", nome:"Billie Jean", track:'../sounds/BillieJean.mp3'}] },
-    { id: 6, nome: "SeuJorge", capa: '../images/6.jpg', musica: [{id:1, cantor: "SeuJorge", nome:"Amiga da minha mulher", track:'../sounds/Amiga_da_minha_mulher.mp3'}] }
+    { id: 1, nome: "Adele", capa: '../images/1.jpg', musica: [
+        {id:1, cantor: "Adele", nome:"set fire to the rain", track:'../sounds/Set_fire_to_the_rain.mp3'},
+        {id:2, cantor: "Desconhecido", nome:"Som", track:'../sounds/file_example_MP3.mp3'},
+        {id:3, cantor: "Desconhecido", nome:"Som", track:'../sounds/file_example_MP3.mp3'}
+    ]},
+    { id: 2, nome: "Queen", capa: '../images/2.jpg', musica: [
+        {id:1, cantor: "Queen",nome:"Bohemian Rhapsody", track:'../sounds/BohemianRhapsody.mp3'},
+        {id:2, cantor: "Desconhecido", nome:"Som", track:'../sounds/file_example_MP3.mp3'},
+        {id:2, cantor: "Desconhecido", nome:"Som", track:'../sounds/file_example_MP3.mp3'}
+    ] },
+    { id: 3, nome: "Coldplay", capa: '../images/3.jpg', musica: [
+        {id:1, cantor: "Coldplay", nome:"Viva La Vida", track:'../sounds/VivaLaVida.mp3'},
+        {id:2, cantor: "Desconhecido", nome:"Som", track:'../sounds/file_example_MP3.mp3'},
+        {id:2, cantor: "Desconhecido", nome:"Som", track:'../sounds/file_example_MP3.mp3'}
+    ] },
+    { id: 4, nome: "Beatles", capa: '../images/4.jpg', musica: [
+        {id:1, cantor: "Beatles", nome:"Here comes the sun", track:'../sounds/HereComesTheSun.mp3'},
+        {id:2, cantor: "Desconhecido", nome:"Som", track:'../sounds/file_example_MP3.mp3'}
+    ] },
+    { id: 5, nome: "Michael Jackson", capa: '../images/5.jpg', musica: [
+        {id:1, cantor: "Michael Jackson", nome:"Billie Jean", track:'../sounds/BillieJean.mp3'},
+        {id:2, cantor: "Desconhecido", nome:"Som", track:'../sounds/file_example_MP3.mp3'},
+        {id:2, cantor: "Desconhecido", nome:"Som", track:'../sounds/file_example_MP3.mp3'},
+        {id:2, cantor: "Desconhecido", nome:"Som", track:'../sounds/file_example_MP3.mp3'}
+    ] },
+    { id: 6, nome: "Seu Jorge", capa: '../images/6.jpg', musica: [
+        {id:1, cantor: "Seu Jorge", nome:"Amiga da minha mulher", track:'../sounds/Amiga_da_minha_mulher.mp3'},
+        {id:2, cantor: "Desconhecido", nome:"Som", track:'../sounds/file_example_MP3.mp3'}
+    ] },
+    { id: 7, nome: "Avicci", capa: '../images/7.jpg', musica: [
+        {id:1, cantor: "Avicci", nome:"The nights", track:'../sounds/Avicii_The_Nights.mp3'}
+    ] },
+    { id: 8, nome: "Imagine Dragons", capa: '../images/8.jpg', musica: [
+        {id:1, cantor: "Imagine Dragons", nome:"Bones", track:'../sounds/Imagine_Dragons_Bones.mp3'},
+        {id:2, cantor: "Desconhecido", nome:"Som", track:'../sounds/file_example_MP3.mp3'},
+        {id:2, cantor: "Desconhecido", nome:"Som", track:'../sounds/file_example_MP3.mp3'}
+    ] }
 ];
 
 export default data;
