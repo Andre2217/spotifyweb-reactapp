4ff0004e6ce44375d3d9c20ea7fcd763248f85d4
Rafael-FHP <rafaelholandafer@edu.unifor.br>
2023-04-07 07:24:37 -0300

    Validação do E-mail na Pagina de Cadastro

--- src/components/Cadastro/Cadastro.jsx
+++ src/components/Cadastro/Cadastro.jsx
@@ -4,26 +4,34 @@ import { useNavigate } from 'react-router-dom';
 function Cadastro() {
 
   const [email, setEmail] = useState('');
+  const [emailConfirmacao, setEmailConfirmacao] = useState('');
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
   const [styles, setStyles] = useState([]);
   const navigate = useNavigate();
 
+
   function handleSubmit(event) {
     event.preventDefault();
-
-    console.log(email);
-    console.log(password);
-    navigate('/login');
-  }
-
-
+    console.log({email, emailConfirmacao, username, password, styles});
+    if (email !== emailConfirmacao) {
+      setError('Os e-mails não correspondem');
+      return;
+    }
+      setError('Cadastro Realizado');
+    navigate();
+    }
+  
     return (
         <div className="container">
           <div className="row justify-content-center">
             <div className="col-md-6">
+              
               <form onSubmit={handleSubmit}>
                 <h1 className="text-center mb-4">Cadastro</h1>
+                
+                
                 <div className="form-group mb-3">
                   <label htmlFor="email">Email</label>
                   <input
@@ -36,6 +44,22 @@ function Cadastro() {
                     required
                   />
                 </div>
+                
+                
+                <div className="form-group mb-3">
+                  <label htmlFor="email-confirmacao">Confirme seu Email</label>
+                  <input
+                    type="email"
+                    className="form-control"
+                    id="emailconfirmacao"
+                    placeholder="Digite seu email novamente"
+                    value={emailConfirmacao}
+                    onChange={(e) => setEmailConfirmacao(e.target.value)}
+                    required
+                  />
+                </div>
+                
+                
                 <div className="form-group mb-3">
                   <label htmlFor="username">Nome de usuário</label>
                   <input
@@ -48,6 +72,8 @@ function Cadastro() {
                     required
                   />
                 </div>
+               
+                
                 <div className="form-group mb-3">
                   <label htmlFor="password">Senha</label>
                   <input
@@ -60,6 +86,8 @@ function Cadastro() {
                     required
                   />
                 </div>
+                
+                
                 <div className="form-group mb-3">
                   <label htmlFor="styles">Estilos musicais favoritos</label>
                   <select
@@ -77,6 +105,8 @@ function Cadastro() {
                     <option value="classical">Clássica</option>
                   </select>
                 </div>
+                
+                
                 <div className="form-group form-check mb-3">
                   <input
                     type="checkbox"
@@ -88,6 +118,8 @@ function Cadastro() {
                     Eu aceito os <a href="https://www.spotify.com/br-pt/legal/end-user-agreement/" target="_blank" rel="noopener noreferrer">termos de compromisso</a>
                   </label>
                 </div>
+                
+                
                 <div className="form-group form-check">
                   <input
                     type="checkbox"
@@ -98,8 +130,11 @@ function Cadastro() {
                     Quero receber novidades e newsletters sobre a TocaPlay
                   </label>
                 </div>
+                
+                
                 <div className="text-center mt-4">
                   <button type="submit" className="btn btn-primary">Cadastrar</button>
+                  <p>{error}</p>
                 </div>
               </form>
               <br></br>
