ce93186b895a9c67cc64185b09a7133054c44261
Andre <andre28ap@gmail.com>
2023-04-26 10:42:31 -0300

    playlist provadas

--- src/components/Perfil/Perfil.jsx
+++ src/components/Perfil/Perfil.jsx
@@ -1,14 +1,31 @@
+import axios from 'axios';
+
 
 function Perfil() {
 
-const usuario = JSON.parse(localStorage.getItem("usuarioLogado"))
-  return (
-    <div>
-      <h1>{usuario.nome}</h1>
-      <h2>{usuario.email}</h2>
-      <button></button>
-    </div>
-  );
+    const usuario = JSON.parse(localStorage.getItem("usuarioLogado"))
+    const id = usuario.id;
+
+
+    function cadastrarPlaylists() {
+        axios.put(`http://localhost:3001/usuarios/${id}`, {
+            nome: usuario.nome,
+            email: usuario.email,
+            senha: usuario.senha,
+            playlits: [{
+                nome: "x",
+                musicas: "y"
+            }]
+        })
+
+    }
+    return (
+        <div>
+            <h1>{usuario.nome}</h1>
+            <h2>{usuario.email}</h2>
+            <button onClick={cadastrarPlaylists}>Cadastrar PLaylists</button>
+        </div>
+    );
 
 
 }
